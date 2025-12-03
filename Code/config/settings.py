e7ee1603e4b9f3ee450497baec6ea422625d4396
sssspppuud <sssspppuud@gmail.com>
2025-12-03 21:44:33 +0000

    Improved charts document

--- Code/config/settings.py
+++ Code/config/settings.py
@@ -4,7 +4,7 @@ DATA_ROOT = "data"
 TRAIN_SUBSET = "train"
 TEST_SUBSET = "test"
 
-FIGURES_ROOT = "Figures"
+FIGURES_ROOT = "out"
 
 PROCESSED_DATA_FILE = os.path.join(DATA_ROOT, "processed_dataset.parquet")
 
